0909a38e483f9d505408c950d14fadf90067c576
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-09-18 19:58:52 -0700

    create if statements for password generator prompts and alert failures

--- Develop/script.js
+++ Develop/script.js
@@ -18,4 +18,22 @@ var value = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%
 var userPasswordLength = 0
 while (userPasswordLength < 8 || userPasswordLength > 128) {
 
+  var userPasswordLengthInput = prompt("What is the password length?")
+  ​
+      if (isNaN(userPasswordLengthInput)) {
+          alert('Please enter an number.')
+  ​
+          continue;
+      }
+      userPasswordLength = parseInt(userPasswordLengthInput)
+
+      if (userPasswordLength < 8 || userPasswordLength > 128) {
+        alert('Please enter a password length from 8-128 characters.')
+
+        continue;
+      }
+
+      if (userCaps = false && userLowercase = false && userNumbers = false && userSymbols = false) {
+      alert ("You must choose at least one prompt to generate a password.")
+      }
 }
