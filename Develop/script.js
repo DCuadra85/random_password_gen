ca1a7addfb69cc56a6fce1b3c76c8abb6e5ca0ec
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-09-19 10:47:08 -0700

    rethink confirm prompts, current code is sloppy // placeholder until research done

--- Develop/script.js
+++ Develop/script.js
@@ -10,28 +10,24 @@ function writePassword() {
 
 }
 
-var valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+ "
+// var valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+ "
 
-for (var i= 0; i < passwordText; i++) {
-  userPasswordLength = password + valueChar.charAt(Math.floor(Math.random() * Math.floor(valueChar.length - 1)))
-}
+
 
 var userPasswordLength = 0
 while (userPasswordLength < 8 || userPasswordLength > 128) {
 
-  var userPasswordLengthInput = prompt("What is the password length?")
-  ​
+  var userPasswordLengthInput = prompt("What is the password length?");
       if (isNaN(userPasswordLengthInput)) {
-          alert('Please enter a number.')
-  ​
+          alert("Please enter a number.")
           continue;
       }
       userPasswordLength = parseInt(userPasswordLengthInput)
 
       if (userPasswordLength < 8 || userPasswordLength > 128) {
-        alert('Please enter a password length from 8-128 characters.')
+        alert("Please enter a password length from 8-128 characters.")
 
-        continue;
+          continue;
       }
 
   var userLowercase = confirm ("Does the password have lower case?");
@@ -39,37 +35,41 @@ while (userPasswordLength < 8 || userPasswordLength > 128) {
   var userNumbers = confirm ("Does the password have numbers?");
   var userSymbols = confirm("Does the password have special characters?");
 
-      if (userCaps = true && userLowercase = true && userNumbers = true && userSymbols = true){
-        valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+ ";
-      }
+      // if (userCaps == true && userLowercase == true && userNumbers == true && userSymbols == true){
+      //   var valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+ ";
+      // }
+    
+      // else (userCaps == true && userLowercase == true && userNumbers == true && userSymbols == false){
+      //   valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+      // }
 
-      else if (userCaps = true && userLowercase = true && userNumbers = true && userSymbols = false){
-        valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-      }
+      // else (userCaps == true && userLowercase == true && userNumbers == false && userSymbols == false){
+      //   valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+      // }
 
-      else if (userCaps = true && userLowercase = true && userNumbers = false && userSymbols = false){
-        valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-      }
+      // else (userCaps == true && userLowercase == false && userNumbers == false && userSymbols == false){
+      //   valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      // }
 
-      else if (userCaps = true && userLowercase = false && userNumbers = false && userSymbols = false){
-        valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      }
+      // else (userCaps == true && userLowercase == false && userNumbers == true && userSymbols == false){
+      //   valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+      // }
 
-      else if (userCaps = true && userLowercase = false && userNumbers = true && userSymbols = false){
-        valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-      }
+      // else (userCaps == true && userLowercase == false && userNumbers == false && userSymbols == true){
+      //   valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+ ";
+      // }
 
-      else if (userCaps = true && userLowercase = false && userNumbers = false && userSymbols = true){
-        valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+ ";
-      }
 
       
-  
-
-      else if (userCaps = false && userLowercase = false && userNumbers = false && userSymbols = false) {
+      else (userCaps == false && userLowercase == false && userNumbers == false && userSymbols == false) {
       alert ("You must choose at least one prompt to generate a password.")
       }
 }
 
+while 
+
+for (var i= 0; i < userPasswordLengthInput; i++) {
+  userPasswordLength = password + valueChar.charAt(Math.floor(Math.random() * Math.floor(valueChar.length - 1)))
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
