cbe19e8fcdaac80100d316f58ce00e8134da645c
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-09-19 13:54:37 -0700

    passwordSelection test, verify in console log success, need to convert strings into viewable password

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 // var valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+ "
 
 var userLowercase = confirm ("Will this password have lower case?");
@@ -39,79 +30,88 @@ while (userPasswordLength < 8 || userPasswordLength > 128) {
       if ((userCaps === true) && (userLowercase === true) && (userNumbers === true) && (userSymbols === true)){
         var valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+ ";
         for (var i= 0; i < userPasswordLengthInput; i++) {
-          var passwordSelection = valueChar[Math.floor(Math.random() * (valueChar.length -1))]
+          var passwordSelection = valueChar[Math.floor(Math.random() * (valueChar.length))]
+          console.log(passwordSelection)
         }
-        document.getElementById("password") 
-        console.log(password)
+
       }
       //tttf
       else if((userCaps === true) && (userLowercase === true) && (userNumbers === true) && (userSymbols === false)){
         valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
         for (var i= 0; i < userPasswordLengthInput; i++) {
-          userPasswordLength = password + valueChar.charAt(Math.floor(Math.random() * Math.floor(valueChar.length - 1)))
+          var passwordSelection = valueChar[Math.floor(Math.random() * (valueChar.length))]
+          console.log(passwordSelection)
         }
       }
       //ttff
       else if((userCaps === true) && (userLowercase === true) && (userNumbers === false) && (userSymbols === false)){
         valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
         for (var i= 0; i < userPasswordLengthInput; i++) {
-          userPasswordLength = password + valueChar.charAt(Math.floor(Math.random() * Math.floor(valueChar.length - 1)))
+          var passwordSelection = valueChar[Math.floor(Math.random() * (valueChar.length))]
+          console.log(passwordSelection)
         }
       }
       //tfff
       else if((userCaps === true) && (userLowercase === false) && (userNumbers === false) && (userSymbols === false)){
         valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         for (var i= 0; i < userPasswordLengthInput; i++) {
-          userPasswordLength = password + valueChar.charAt(Math.floor(Math.random() * Math.floor(valueChar.length - 1)))
+          var passwordSelection = valueChar[Math.floor(Math.random() * (valueChar.length))]
+          console.log(passwordSelection)
         }
       }
       //tfft
       else if((userCaps === true) && (userLowercase === false) && (userNumbers === false) && (userSymbols === true)){
         valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+ ";
         for (var i= 0; i < userPasswordLengthInput; i++) {
-          userPasswordLength = password + valueChar.charAt(Math.floor(Math.random() * Math.floor(valueChar.length - 1)))
+          var passwordSelection = valueChar[Math.floor(Math.random() * (valueChar.length))]
+          console.log(passwordSelection)
         }
       }
       //tftf
       else if((userCaps === true) && (userLowercase === false) && (userNumbers === true) && (userSymbols === false)){
         valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
         for (var i= 0; i < userPasswordLengthInput; i++) {
-          userPasswordLength = password + valueChar.charAt(Math.floor(Math.random() * Math.floor(valueChar.length - 1)))
+          var passwordSelection = valueChar[Math.floor(Math.random() * (valueChar.length))]
+          console.log(passwordSelection)
         }
       }
       //tftt
       else if((userCaps === true) && (userLowercase === false) && (userNumbers === true) && (userSymbols === true)){
         valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+ ";
-        for (var i= 0; i < userPasswordLengthInputr; i++) {
-          userPasswordLength = password + valueChar.charAt(Math.floor(Math.random() * Math.floor(valueChar.length - 1)))
+        for (var i= 0; i < userPasswordLengthInput; i++) {
+          var passwordSelection = valueChar[Math.floor(Math.random() * (valueChar.length))]
+          console.log(passwordSelection)
         }
       }
       //ttft
       else if((userCaps === true) && (userLowercase === true) && (userNumbers === false) && (userSymbols === true)){
         valueChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+ ";
-        for (var i= 0; i < userPasswordLengthInputr; i++) {
-          userPasswordLength = password + valueChar.charAt(Math.floor(Math.random() * Math.floor(valueChar.length - 1)))
+        for (var i= 0; i < userPasswordLengthInput; i++) {
+          var passwordSelection = valueChar[Math.floor(Math.random() * (valueChar.length))]
+          console.log(passwordSelection)
         }
       }
       //fttt
       else if((userCaps === false) && (userLowercase === true) && (userNumbers === true) && (userSymbols === true)){
         valueChar = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+ ";
         for (var i= 0; i < userPasswordLengthInput; i++) {
-          userPasswordLength = password + valueChar.charAt(Math.floor(Math.random() * Math.floor(valueChar.length - 1)))
+          var passwordSelection = valueChar[Math.floor(Math.random() * (valueChar.length))]
+          console.log(passwordSelection)
         }
       }
       //fftt
       else if((userCaps === false) && (userLowercase === false) && (userNumbers === true) && (userSymbols === true)){
         valueChar = "1234567890!@#$%^&*()_+ ";
         for (var i= 0; i < userPasswordLengthInput; i++) {
-          userPasswordLength = password + valueChar.charAt(Math.floor(Math.random() * Math.floor(valueChar.length - 1)))
-        }
+          var passwordSelection = valueChar[Math.floor(Math.random() * (valueChar.length))]
+          console.log(passwordSelection)
       }
       //ffft
       else if((userCaps === false) && (userLowercase === false) && (userNumbers === false) && (userSymbols === true)){
         valueChar = "!@#$%^&*()_+ ";
         for (var i= 0; i < userPasswordLengthInput; i++) {
-          userPasswordLength = password + valueChar.charAt(Math.floor(Math.random() * Math.floor(valueChar.length - 1)))
+          var passwordSelection = valueChar[Math.floor(Math.random() * (valueChar.length))]
+          console.log(passwordSelection)
         }
 
       }
@@ -119,6 +119,18 @@ while (userPasswordLength < 8 || userPasswordLength > 128) {
       else if((userCaps === false) && (userLowercase === false) && (userNumbers === false) && (userSymbols === false)) {
       alert ("You must choose at least one prompt to generate a password.")
       }
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordSelection = document.querySelector("#password");
+
+  passwordSelection.value = userPassword;
+
+}
+
+
 
 
 //example of the RNG array function
